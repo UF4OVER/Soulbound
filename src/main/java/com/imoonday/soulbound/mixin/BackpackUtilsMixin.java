fa7b59f5918f9e6ea207d466437f9e1a8a193b95
iMoonDay <562613736@qq.com>
2024-07-03 17:36:29 +0800

    v1.0.0

--- src/main/java/com/imoonday/soulbound/mixin/BackpackUtilsMixin.java
+++ src/main/java/com/imoonday/soulbound/mixin/BackpackUtilsMixin.java
@@ -1,8 +1,8 @@
 package com.imoonday.soulbound.mixin;
 
-import com.imoonday.soulbound.SoulBound;
+import com.beansgalaxy.backpacks.events.LivingEntityDeath;
+import com.imoonday.soulbound.SoulBoundEnchantment;
 import com.tiviacz.travelersbackpack.util.BackpackUtils;
-import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
@@ -13,9 +13,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BackpackUtils.class)
 public class BackpackUtilsMixin {
+
     @Inject(method = "onPlayerDeath", at = @At("HEAD"), cancellable = true)
     private static void handleOnPlayerDeath(World world, PlayerEntity player, ItemStack stack, CallbackInfo ci) {
-        if (EnchantmentHelper.getLevel(SoulBound.SOUL_BOUND, stack) > 0) {
+        if (SoulBoundEnchantment.hasSoulbound(stack)) {
             ci.cancel();
         }
     }
