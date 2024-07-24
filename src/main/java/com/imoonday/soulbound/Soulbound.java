94ea0fedfa21e12ad9ea5f389d61ba210b6ee821
iMoonDay <562613736@qq.com>
2024-07-24 14:33:11 +0800

    临时备份

--- src/main/java/com/imoonday/soulbound/Soulbound.java
+++ src/main/java/com/imoonday/soulbound/Soulbound.java
@@ -2,7 +2,9 @@ package com.imoonday.soulbound;
 
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
@@ -27,4 +29,8 @@ public class Soulbound {
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
+    @SubscribeEvent
+    public void onClone(PlayerEvent.Clone e) {
+        SoulBoundEnchantment.copySoulBoundItems(e.getOriginal(), e.getEntity(), e.isWasDeath());
+    }
 }
